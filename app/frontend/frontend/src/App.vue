74a18a859a25a0dba86d6829c9632c544848f1c7
1001-Martinez-Jose <Joserafaelflores07@gmail.com>
2022-12-04 03:54:47 -0800

    sidebar component and import for free icons

--- app/frontend/frontend/src/App.vue
+++ app/frontend/frontend/src/App.vue
@@ -1,5 +1,19 @@
+<script>
+import Sidebar from "./components/Sidebar";
+import { sidebarWidth } from "./components/state";
+export default {
+  components: { Sidebar },
+  setup() {
+    return { sidebarWidth };
+  },
+};
+</script>
+
 <template>
-  <router-view />
+  <Sidebar />
+  <div :style="{ 'margin-left': sidebarWidth }">
+    <router-view />
+  </div>
 </template>
 
 <style>
