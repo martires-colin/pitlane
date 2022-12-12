98a838a8a1ef24ce3c8d111decdd70aad81ab740
Colin <cmartires@nevada.unr.edu>
2022-12-11 19:30:44 -0800

    styled login/create-accounts pages

--- app/frontend/frontend/src/views/CreateAccount.vue
+++ app/frontend/frontend/src/views/CreateAccount.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <!-- <main>
     <h1>Create Account</h1>
 
     <section class="forms">
@@ -20,7 +20,67 @@
     <a href="/login">Already have an account?</a>
 
     </section>
-  </main>
+  </main> -->
+
+
+
+<section class="vh-100 vw-100" style="background-color: #060326;">
+  <div class="container  h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-12">
+        <div class="card" style="border-radius: 1rem; border: none;">
+          <div class="row g-0">
+            <div class="col-md-6 col-lg-5 d-none d-md-block">
+              <img src="../assets/create-account-pic.jpg"
+                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+            </div>
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
+
+                <form class="login" @submit.prevent="register">
+
+                  <div class="d-flex align-items-center justify-content-center mb-3 pb-1">
+                    <img
+                      src="@/assets/PL.png"
+                      alt="Website logo"
+                      width="150"
+                      height="150"
+                    />
+                  </div>
+
+                  <h5 class="fw-normal mb-0 pb-3" style="letter-spacing: 1px;">Create an account</h5>
+
+                  <div class="form-outline mb-4">
+                    <label class="form-label">Email address</label>
+                    <input type="email" placeholder="Email" class="form-control form-control-lg" v-model="register_form.email"/>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <label class="form-label">Password</label>
+                    <input type="password" placeholder="Password" class="form-control form-control-lg" v-model="register_form.password"/>
+                  </div>
+
+                  <div class="pt-1 mb-4">
+                    <button class="btn btn-dark btn-lg btn-block" type="submit">Login</button>
+                  </div>
+
+                  <!-- <a class="small text-muted" href="#!">Forgot password?</a> -->
+                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Already have an account? <a href="/login"
+                      style="color: #393f81;">Login here</a></p>
+                  <!-- <a href="#!" class="small text-muted">Terms of use.</a>
+                  <a href="#!" class="small text-muted">Privacy policy</a> -->
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
 </template>
 
 <script>
--- app/frontend/frontend/src/views/Login.vue
+++ app/frontend/frontend/src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <main>
+  <!-- <main>
     <h1>Login Page</h1>
 
     <section class="forms">
@@ -20,7 +20,64 @@
     </section>
 
     <a href="/create-account">Create an Account</a>
-  </main>
+  </main> -->
+
+<section class="vh-100 vw-100" style="background-color: #060326;">
+  <div class="container  h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-12">
+        <div class="card" style="border-radius: 1rem; border: none;">
+          <div class="row g-0">
+            <div class="col-md-6 col-lg-5 d-none d-md-block">
+              <img src="../assets/login-pic.jpg"
+                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+            </div>
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
+
+                <form class="login" @submit.prevent="login">
+
+                  <div class="d-flex align-items-center justify-content-center mb-3 pb-1">
+                    <img
+                      src="@/assets/PL.png"
+                      alt="Website logo"
+                      width="150"
+                      height="150"
+                    />
+                  </div>
+
+                  <h5 class="fw-normal mb-0 pb-3" style="letter-spacing: 1px;">Sign into your account</h5>
+
+                  <div class="form-outline mb-4">
+                    <label class="form-label">Email address</label>
+                    <input type="email" placeholder="Email" class="form-control form-control-lg" v-model="login_form.email"/>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <label class="form-label">Password</label>
+                    <input type="password" placeholder="Password" class="form-control form-control-lg" v-model="login_form.password"/>
+                  </div>
+
+                  <div class="pt-1 mb-4">
+                    <button class="btn btn-dark btn-lg btn-block" type="submit">Login</button>
+                  </div>
+
+                  <!-- <a class="small text-muted" href="#!">Forgot password?</a> -->
+                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="/create-account"
+                      style="color: #393f81;">Register here</a></p>
+                  <!-- <a href="#!" class="small text-muted">Terms of use.</a>
+                  <a href="#!" class="small text-muted">Privacy policy</a> -->
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 </template>
 
 <script>
