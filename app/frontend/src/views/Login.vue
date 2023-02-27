73969148bd61aa2ca28682aa7145c5fb719d30d9
martires-colin <cmartires@nevada.unr.edu>
2023-02-27 10:52:55 -0800

    fixed img sizing

--- app/frontend/src/views/Login.vue
+++ app/frontend/src/views/Login.vue
@@ -2,14 +2,14 @@
 
 <template>
 
-  <div class="container  h-100">
+  <div class="container h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col col-xl-12">
-        <div class="card" style="border-radius: 1rem; border: none;">
+        <div class="card m-5" style="border-radius: 1rem; border: none;">
           <div class="row g-0">
             <div class="col-md-6 col-lg-5 d-none d-md-block">
               <img src="../assets/login-pic.jpg"
-                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                alt="login form" class="h-100 img-fluid" style="border-radius: 1rem 0 0 1rem;" />
             </div>
             <div class="col-md-6 col-lg-7 d-flex align-items-center">
               <div class="card-body p-4 p-lg-5 text-black">
