b6d4a7001b49b48ce6345c55b70f65c625fcedc2
Colin <cmartires@nevada.unr.edu>
2023-04-04 00:21:11 -0700

    push without firebase config file

--- app/frontend/src/firebase/index.js
+++ app/frontend/src/firebase/index.js
@@ -5,8 +5,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
+require("dotenv").config();
+
 //Firebase configuration
 const firebaseConfig = {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APP_ID,
   apiKey: "AIzaSyB8fx4XTZBGanb2GzcAyiZDae-6cPe9Yfc",
   authDomain: "pitlane-ad39e.firebaseapp.com",
   projectId: "pitlane-ad39e",
